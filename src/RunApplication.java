32babfc115337c1f3bf932fc54aa523bf7b1ce9a
voldi <vova.ilyshenko2016@gmail.com>
2022-09-14 20:37:58 +0300

    Last Version

--- src/RunApplication.java
+++ src/RunApplication.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class TestClientSocket {
+public class RunApplication {
     public static void main(String[] args) throws IOException {
         FTpClient ftpClient = new FTpClient();
         ftpClient.mainLoop();
